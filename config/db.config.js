9a8a44a47dd847bb17c39a906f37d15e2a9c3a7b
Pranil Kharche <pranilkharche@gmail.com>
2021-03-22 14:10:15 +0530

    boi

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://pkay:pkay@cluster0.qbvvg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
---
+++ models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose => {
+  const Tutorial = mongoose.model(
+    "tutorial",
+    mongoose.Schema(
+      {
+        title: String,
+        description: String,
+        published: Boolean
+      },
+      { timestamps: true }
+    )
+  );
+
+  return Tutorial;
+};
